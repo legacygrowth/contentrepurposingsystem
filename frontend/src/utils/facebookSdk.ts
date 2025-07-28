86083ab2f6cc0ef06cf6c29586b992bb25e7866a
Moughees110 <moughees@360synergytech.com>
2025-07-29 00:43:20 +0500

    testing deployment

--- frontend/src/utils/facebookSdk.ts
+++ frontend/src/utils/facebookSdk.ts
@@ -1,34 +1,34 @@
-declare global {
-  interface Window {
-    FB: any;
-    fbAsyncInit: () => void;
-  }
-}
+// declare global {
+//   interface Window {
+//     FB: any;
+//     fbAsyncInit: () => void;
+//   }
+// }
 
-export const loadFacebookSDK = (): Promise<void> => {
-  return new Promise((resolve) => {
-    if (window.FB) {
-      resolve();
-      return;
-    }
+// export const loadFacebookSDK = (): Promise<void> => {
+//   return new Promise((resolve) => {
+//     if (window.FB) {
+//       resolve();
+//       return;
+//     }
 
-    window.fbAsyncInit = function () {
-      window.FB.init({
-        appId: '1417585822997480',
-        cookie: true,
-        xfbml: false,
-        version: 'v20.0',
-      });
-      resolve();
-    };
+//     window.fbAsyncInit = function () {
+//       window.FB.init({
+//         appId: '1417585822997480',
+//         cookie: true,
+//         xfbml: false,
+//         version: 'v20.0',
+//       });
+//       resolve();
+//     };
 
-    (function (d, s, id) {
-      const fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      const js = d.createElement(s) as HTMLScriptElement;
-      js.id = id;
-      js.src = 'https://connect.facebook.net/en_US/sdk.js';
-      fjs.parentNode?.insertBefore(js, fjs);
-    })(document, 'script', 'facebook-jssdk');
-  });
-};
+//     (function (d, s, id) {
+//       const fjs = d.getElementsByTagName(s)[0];
+//       if (d.getElementById(id)) return;
+//       const js = d.createElement(s) as HTMLScriptElement;
+//       js.id = id;
+//       js.src = 'https://connect.facebook.net/en_US/sdk.js';
+//       fjs.parentNode?.insertBefore(js, fjs);
+//     })(document, 'script', 'facebook-jssdk');
+//   });
+// };
